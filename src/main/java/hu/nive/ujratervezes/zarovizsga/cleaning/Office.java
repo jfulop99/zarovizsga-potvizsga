f6246b7288e7833281a8c5384dea547ff5c00a7f
jfulop99 <janos.fulop.mail@gmail.com>
2021-03-22 17:36:28 +0100

    Takarítás

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/cleaning/Office.java
@@ -0,0 +1,27 @@
+package hu.nive.ujratervezes.zarovizsga.cleaning;
+
+public class Office implements Cleanable{
+
+    public static final int OFFICE_PRICE_PER_SQRM = 100;
+    private final String address;
+
+    private final int area;
+
+    private final int levels;
+
+    public Office(String address, int area, int levels) {
+        this.address = address;
+        this.area = area;
+        this.levels = levels;
+    }
+
+    @Override
+    public int clean() {
+        return levels * area * OFFICE_PRICE_PER_SQRM;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+}
